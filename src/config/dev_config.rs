df3173611c9c875ccbbf189ed84b734980870bfe
sato <ysudqfs@163.com>
2023-04-26 00:42:55 +0800

    test

--- src/config/dev_config.rs
+++ src/config/dev_config.rs
@@ -4,13 +4,3 @@ pub const LCD_CS: u8 = GPIOCHIP_BASE + 49;
 pub const LCD_RST: u8 = GPIOCHIP_BASE + 42;
 pub const LCD_DC: u8 = GPIOCHIP_BASE + 44;
 pub const LCD_BL: u8 = GPIOCHIP_BASE + 51;
-
-
-fn fill_screen(spi: &mut Spidev, cs: &Pin, dc: &Pin, color: u8) {
-    set_pos(spi, cs, dc, 0, 0);
-    cs.set_value(0).unwrap();
-    for _ in 0..(128 * 64 / 8) {
-        spi.write(&[color]).unwrap();
-    }
-    cs.set_value(1).unwrap();
-}
