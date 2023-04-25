a436a6c01cd986ce79d8e91cf3fa713f2d067644
sato <ysudqfs@163.com>
2023-04-25 14:31:25 +0800

    fix

--- src/examples/lcd2inch.rs
+++ src/examples/lcd2inch.rs
@@ -1,4 +1,5 @@
 use waveshare_lcd::config::dev_hardware_spi::HardwareSpi;
+use waveshare_lcd::gui::types::WHITE;
 use waveshare_lcd::lcd::lcd_2inch::LCD;
 use waveshare_lcd::lcd::types::Inch;
 
@@ -7,5 +8,5 @@ fn main() {
     let spi = HardwareSpi::new("/dev/spidev1.0");
     let mut lcd = LCD::new(Inch::Lcd2inch {width:320, height:240 },  spi);
     lcd.lcd_in_init();
-    lcd.lcd_2in_clear()
+    lcd.lcd_2in_clear(WHITE)
 }
